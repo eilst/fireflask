c627593c9af9d80674fc749be4421bd66c4d938f
lagrg <a01337755@gmail.com>
2017-10-10 00:40:43 -0500

    CORS

--- load_catalog.py
+++ load_catalog.py
@@ -12,15 +12,16 @@ config = {
 }
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-def populate(csvfile):
-    data = csv.reader(open(csvfile, encoding="latin-1"))
-    fields = next(data)
+ i = 1
     for row in data:
         """remove any character  []#/.$ in key ==> firebase requirement """
         item = {re.sub('[#/.$]', '', row[2]) : row[2]}
         """update() will not duplicate any item"""
         db.child(row[0]).child(row[1]).update(item)
-        print(data)
+        """lines in my test file is 881"""
+        sys.stdout.write("Upload progress: %d%%   \r" % (int((i/881)*100)))
+        sys.stdout.flush()
+        i=i+1
 
 
 populate("catalogo.csv")
